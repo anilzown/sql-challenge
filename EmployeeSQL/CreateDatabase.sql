20a69eccd66f092a37e6556393f01f81b2afb159
Anil Kumar <>
2021-08-17 00:32:28 -0400

    modified:   EmployeeSQL/CreateDatabase.sql 	modified:   EmployeeSQL/Database-ERD.png 	modified:   EmployeeSQL/QueryDatabase.sql

--- EmployeeSQL/CreateDatabase.sql
+++ EmployeeSQL/CreateDatabase.sql
@@ -24,11 +24,11 @@ CREATE TABLE "Dept_Manager" (
 CREATE TABLE "Employees" (
     "emp_no" INTEGER   NOT NULL,
     "emp_title_id" VARCHAR   NOT NULL,
-    "birth_date" VARCHAR   NOT NULL,
+    "birth_date" DATE   NOT NULL,
     "first_name" VARCHAR   NOT NULL,
     "last_name" VARCHAR   NOT NULL,
     "sex" VARCHAR   NOT NULL,
-    "hire_date" VARCHAR   NOT NULL,
+    "hire_date" DATE   NOT NULL,
     CONSTRAINT "pk_Employees" PRIMARY KEY (
         "emp_no"
      )
--- EmployeeSQL/QueryDatabase.sql
+++ EmployeeSQL/QueryDatabase.sql
@@ -1,22 +1,23 @@
 -- List the following details of each employee: employee number, last name, first name, sex, and salary.
-
+select e.emp_no, e.last_name, e.first_name, e.sex, s.salary from "Employees" e inner join "Salaries" s on e.emp_no = s.emp_no
 
 -- List first name, last name, and hire date for employees who were hired in 1986.
-
+select e.first_name, e.last_name, e.hire_date from "Employees" e  where e.hire_date >= '1986-01-01' AND e.hire_date <= '1986-12-31';
 
 -- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
-
+select d.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name from "Departments" d join "Dept_Manager" dm on  dm.dept_no =  d.dept_no join "Employees" e on dm.emp_no = e.emp_no
 
 -- List the department of each employee with the following information: employee number, last name, first name, and department name.
-
+select e.emp_no, e.last_name, e.first_name, d.dept_name from "Employees" e join "Dept_Emp" de on de.emp_no = e.emp_no join "Departments" d on de.dept_no = d.dept_no
 
 -- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
-
+select e.first_name, e.last_name , e.sex from "Employees" e where e.first_name = 'Hercules' and e.last_name like 'B%'
 
 -- List all employees in the Sales department, including their employee number, last name, first name, and department name.
-
+select e.emp_no, e.last_name, e.first_name , d.dept_name from "Employees" e join "Dept_Emp" de on de.emp_no = e.emp_no join "Departments" d on de.dept_no = d.dept_no where d.dept_name='Sales'
 
 -- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
-
+select e.emp_no, e.last_name, e.first_name , d.dept_name from "Employees" e join "Dept_Emp" de on de.emp_no = e.emp_no join "Departments" d on de.dept_no = d.dept_no where d.dept_name in ('Sales','Development' )
 
 -- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+select e.last_name, count(e.last_name) as frequencyCount from "Employees" e group By e.last_name order by frequencyCount desc
